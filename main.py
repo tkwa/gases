0bace3eb5d100247402ee62de54c327d6cc7473c
Thomas Kwa <kwathomas0@gmail.com>
2024-10-11 11:59:50 -0700

    Added UManSysProp for Nannoolal method

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 # %%
 
 import rdkit
+from rdkit import Chem
 import rdkit.Chem.Descriptors as Descriptors
+from rdkit.Chem.Descriptors import rdMolDescriptors
 from tqdm import tqdm
 from tqdm.contrib.concurrent import process_map  # or thread_map
 import matplotlib.pyplot as plt
 import pandas as pd
 from thermo.group_contribution.joback import Joback
 import importlib
+import UManSysProp_public.umansysprop.boiling_points as bp
+from openbabel import pybel
+import chemicals
 
 from multiprocessing import  Pool
 
@@ -21,7 +26,7 @@ from multiprocessing import  Pool
 directory = 'gdb11'
 
 df = pd.DataFrame()
-for i in range(1, 12):
+for i in range(1, 10):
     data_this_length = pd.read_csv(f'{directory}/gdb11_size{i:02d}.smi',
                                    sep=r"\s+", header=None)
     # set headers
@@ -34,34 +39,46 @@ for i in range(1, 12):
 def process(x):
 
     smiles, length = x
-    ret = {}
     molwt = float('nan')
+    Tb = float('nan')
+    is_gas = False
 
     # no gases have >7 non-F atoms
     if length - smiles.count('F') > 7:
         is_gas = False
         Tb = float('nan')
-    else:
-        mol = rdkit.Chem.MolFromSmiles(smiles)
-        molwt = Descriptors.ExactMolWt(mol)
-        if molwt > 300:  # heaviest known gas is 294
-            is_gas = False
-            Tb = float('nan')
-        else:
-            j = Joback(smiles)
-            Tb = j.Tb(j.counts)
-            is_gas = Tb < 298
+        return {'molwt': molwt, 'is_gas': is_gas, 'Tb': Tb, 'method': 'non-F atoms'}
+    
+    mol = rdkit.Chem.MolFromSmiles(smiles)
+    molwt = Descriptors.ExactMolWt(mol)
+    if molwt > 300:  # heaviest known gas is 294
+        is_gas = False
+        Tb = float('nan')
+        return {'molwt': molwt, 'is_gas': is_gas, 'Tb': Tb, 'method': 'molwt'}
+    
+    try:
+        cas = chemicals.identifiers.CAS_from_any(smiles, autoload=False, cache=True)
+        method = 'YAWS'
+        Tb = chemicals.phase_change.Tb(cas, method=method)
+        is_gas = Tb < 298
+        return {'molwt': molwt, 'is_gas': is_gas, 'Tb': Tb, 'method': method}
+    except:
+        mol = pybel.readstring("smi", smiles)
+        Tb = bp.nannoolal(mol)
+        is_gas = Tb < 298
 
-    ret['molwt'] = molwt
-    ret['is_gas'] = is_gas
-    ret['Tb'] = Tb
+    ret = {'molwt': molwt, 'is_gas': is_gas, 'Tb': Tb, 'method': 'nannoolal'}
     return ret
-
+# %%
 # Use itertuples to create an efficient iterable
 iterable = df[['smiles', 'length']].itertuples(index=False, name=None)
 
-# Update the process_map call
 bp_data = process_map(process, iterable, max_workers=24, chunksize=4000, total=len(df))
+
+# Print method distribution
+method_counts = pd.Series([x['method'] for x in bp_data]).value_counts()
+print(method_counts)
+
 
 # %%
 df = pd.concat([df, pd.DataFrame(bp_data)], axis=1)
@@ -76,15 +93,18 @@ plt.axvline(x=298, color='r')
 plt.show()
 
 print(f"Number of gases: {len(df[df['Tb'] < 298])} of {len(df)}")
-print(f"Used Joback method for {len(df.dropna())} of {len(df)}")
+print(f"Used Nannoolal method for {len(df.dropna())} of {len(df)}")
 
 # row 2344
 # %%
 
 gases_only = df[df['is_gas']]
 
+# random sample 400
+gases_only = gases_only.sample(400)
+
 ims = []
-for i, row in gases_only.iterrows():
+for i, row in tqdm(gases_only.iterrows()):
     print(row.smiles)
     print(f"Est. Tb={row.Tb:.2f} K")
     print(f"MW={row.molwt}")
@@ -95,6 +115,7 @@ for i, row in gases_only.iterrows():
     print()
 
 # %%
+
 
 # Put all images in a grid
 n = len(ims)
